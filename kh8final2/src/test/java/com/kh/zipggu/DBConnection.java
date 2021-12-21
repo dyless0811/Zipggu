1ec112b23a586031e0736253ba0ace5e9fdce2f3
hongbba <88869198+hongbba@users.noreply.github.com>
2021-12-21 17:19:49 +0900

    DB연결 테스트

---
+++ kh8final2/src/test/java/com/kh/zipggu/DBConnection.java
@@ -0,0 +1,28 @@
+package com.kh.zipggu;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+		})
+@WebAppConfiguration
+public class DBConnection {
+
+		@Autowired
+		private SqlSession sqlSession;
+		
+		@Test
+		public void test() {
+			System.out.println(sqlSession);
+	
+		}
+
+}
