3255dde19473a07a1fd8d853a78913d1a16a41f0
hongbba <88869198+hongbba@users.noreply.github.com>
2021-12-24 10:57:14 +0900

    Dto 이동

---
+++ kh8final2/src/main/java/com/kh/zipggu/controller/SnsController.java
@@ -0,0 +1,14 @@
+package com.kh.zipggu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sns")
+public class SnsController {
+	
+	@RequestMapping("/list")
+	public String sns() {
+		return "sns/list";
+	}
+}
---
+++ kh8final2/src/main/webapp/WEB-INF/views/sns/list.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<h1>sns 목록 페이지</h1>
