969c505cbe5794780ed66d9a3be1ea50ec9cb16b
hongbba <88869198+hongbba@users.noreply.github.com>
2022-01-07 01:49:08 +0900

    test

---
+++ kh8final2/src/main/java/com/kh/zipggu/repository/OrderDetailDaoImpl.java
@@ -0,0 +1,55 @@
+package com.kh.zipggu.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.zipggu.entity.OrderDetailDto;
+
+@Repository
+public class OrderDetailDaoImpl implements OrderDetailDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public void insert(OrderDetailDto orderDetailDto) {
+		sqlSession.insert("orderDetail.insert", orderDetailDto);
+	}
+
+	@Override
+	public List<OrderDetailDto> list(int orderNo) {
+		return sqlSession.selectList("orderDetail.listByBuyNo", orderNo);
+	}
+
+	@Override
+	public OrderDetailDto get(int orderNo, int itemNo) {
+		Map<String, Object> param = new HashMap<>();
+		param.put("orderNo", orderNo);
+		param.put("itemNo", itemNo);
+		return sqlSession.selectOne("orderDetail.get", param);
+	}
+
+	@Override
+	public void cancel(int orderNo, int itemNo) {
+		Map<String, Object> param = new HashMap<>();
+		param.put("orderNo", orderNo);
+		param.put("itemNo", itemNo);
+		sqlSession.update("orderDetail.cancel", param);
+	}
+
+	@Override
+	public int getCancelAvailableAmount(int orderNo) {
+		return sqlSession.selectOne("orderDetail.cancelAvailableAmount", orderNo);
+	}
+
+	@Override
+	public void cancelAll(int orderNo) {
+		sqlSession.update("orderDetail.cancelAll", orderNo);
+	}
+
+}
---
+++ kh8final2/src/main/java/com/kh/zipggu/vo/kakaopay/CardInfo.java
@@ -0,0 +1,24 @@
+package com.kh.zipggu.vo.kakaopay;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CardInfo {
+	private String purchase_corp;
+	private String purchase_corp_code;
+	private String issuer_corp;
+	private String issuer_corp_code;
+	private String kakaopay_purchase_corp;
+	private String kakaopay_purchase_corp_code;
+	private String kakaopay_issuer_corp;
+	private String kakaopay_issuer_corp_code	;
+	private String bin;
+	private String card_type;
+	private String install_month;
+	private String approved_id;
+	private String card_mid;
+	private String interest_free_install;
+	private String card_item_code;
+}
